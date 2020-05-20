0fb4a580e22070266a494b0eb7e1d4514e4795bb
mhkmcp <mhkmcp@yahoo.com>
2020-05-20 17:23:39 +0600

    Day 2 - Numbers, Variables & Strings

---
+++ Day 2 Numbers, Variable, Strings/main.py
@@ -0,0 +1,33 @@
+# Numbers
+
+add = 20 + 34982349849
+print('Add: ', add)
+
+subtraction = 4389489 - 37873
+print('Subtraction: ', subtraction)
+
+multiply = 4894 * 2928
+print('Multiplication: ', multiply)
+
+division = 4894 / 23498394
+print('Division: ', division)
+
+expo = 3 ** 4
+print('Exponential: ', expo)
+
+sq_root = 5 ** (1/2)
+print('Square Root: ', sq_root)
+
+
+# Strings
+
+x = "3" * 10
+print(x)
+
+x = "hello world! " * 5
+print(x)
+
+quote = "this is very important qoute by \"Albert Einstein\" one of the greatest scientist"
+print(quote)
+
+print("hello\nworld!")
